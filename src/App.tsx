126e4d04af418da6e3cd88dc90300cc8e89e52ae
Obafemi Fadairo <obafad94@gmail.com>
2022-06-28 00:03:46 -0400

    start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Astronomy Picture of the Day</p>
+
+        {/* link: https://api.nasa.gov/planetary/apod?api_key=FeONMuyEyNh9VfhKSPRFtD9QdKM01D3LKOyEDhjF */}
         <a
           className="App-link"
           href="https://reactjs.org"
