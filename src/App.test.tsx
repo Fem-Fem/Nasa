b6c9b1d9ab49abbcaa1b3d3c2773f392a6d0a870
Obafemi Fadairo <obafad94@gmail.com>
2022-06-28 21:05:28 -0400

    fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders header", () => {
   render(<App />);
-  const linkElement = screen.getByText("Astronomy Picture of the Day");
+  const linkElement = screen.getByText("Astronomy Picture!");
   expect(linkElement).toBeInTheDocument();
 });
